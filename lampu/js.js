ace61b51fe10467904daa0c6b252f4c00f10b4b8
yazid__21 <159028339+Yazid-21@users.noreply.github.com>
2024-02-07 17:07:18 +0800

    lampu-web

---
+++ lampu/js.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById(`btn`);
+const light = document.getElementById(`light`);
+
+function klick() {
+  btn.classList.toggle(`active`);
+  light.classList.toggle(`on`);
+}
